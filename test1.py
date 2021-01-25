a8cb7cfc4586c45a9989406bbc77c052747c6afa
dima7665 <dima7685@gmail.com>
2021-01-25 15:52:57 +0200

    змінив таблицю

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
@@ -10,9 +10,21 @@ rob_zm = ['22','','asd']
 def home():
     return render_template('home.html', title='Ламінування', rob_zm=rob_zm)
 
+
+@app.route("/home2")
+def home2():
+    return render_template('home2.html', title='Ламінування', rob_zm=rob_zm)
+
 @app.route("/robota")
 def robota():
     return render_template('robota.html', title='Робота зміни')
+
+
+@app.route("/test", methods=["POST"])
+def test():
+    n = request.form.getlist("f1")
+    m = request.form.get("f2text")
+    return render_template("test.html", n=n,m=m)
 
 
 
