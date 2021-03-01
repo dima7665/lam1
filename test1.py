731131fd5fae833e528d5635f39531ab01c61489
dima7665 <dima7685@gmail.com>
2021-03-01 15:03:19 +0200

    robota_zm --- 16-18 and maister-zmina

--- test1.py
+++ test1.py
@@ -15,7 +15,7 @@ column_names = dict()
 for i in ['robota_zm']:                     # додати в список інші таблиці
     column_names[i] = get_column_names(i)
 
-def get_pressE(i):
+def get_pressE(i, thick):
     if i in [1,2,3,4,5,6,7]:
         p = 1
     elif i in [8,9,10,11,12,13,14]: 
@@ -24,15 +24,15 @@ def get_pressE(i):
         e = 1
     elif i in [5,6,7,12,13,14]:
         e = 2
-    return [p,e,i]
+    return [p,e,thick,i]
 
 def update_command(table, names_lst, lst):
     if table == 'robota_zm':   
         command = "UPDATE robota_zm SET"
-        for x in range(4,37):
+        for x in range(5,38):
             command += f" {names_lst[x+1]} = '{lst[x]}',"
         command = command[:-1]
-        command += f" WHERE zmina_id = {lst[0]} AND textures_id = {lst[1]} AND press = {lst[2]} AND e_quality = {lst[3]}"
+        command += f" WHERE zmina_id = {lst[0]} AND textures_id = {lst[1]} AND press = {lst[2]} AND e_quality = {lst[3]} AND thickness={lst[4]}"
         return command
 
 
@@ -42,12 +42,14 @@ def home():
     return render_template('home.html', title='Ламінування')
 
 
-@app.route("/home2", methods=["POST","GET"])
+@app.route("/home2", methods=["POST", "GET"])
 @app.route("/home16", methods=["POST","GET"])
+@app.route("/home18", methods=["POST","GET"])
 def home2():
     global gen_info
     if request.method == "POST":
         t_lists = []
+        thick = request.form.get('thickness')
         cur_textures = request.form.getlist("texture")
         zinfo = request.form.getlist("zm_info")
         conn = sqlite3.connect("db/lam1.db")
@@ -57,22 +59,23 @@ def home2():
         nomer_zm = request.form.get('zm_nomer')
 
         # checking for 'zmina' and insert new if needed       
-        cur.execute(f"SELECT zmina_id FROM zmina WHERE zm_date='{zinfo[0]}' AND zm_zmina='{zinfo[1]}'")
+        cur.execute(f"SELECT zmina_id, nomer_zm, maister_id FROM zmina WHERE zm_date='{zinfo[0]}' AND zm_zmina='{zinfo[1]}'")
         zm_id = cur.fetchone()
         if zm_id:
-            zm_id = zm_id[0]
+            if zm_id[2] != maister_id or nomer_zm != zm_id[1]:
+                cur.execute(f"UPDATE zmina SET nomer_zm='{nomer_zm}', maister_id='{maister_id}' WHERE zm_date='{zinfo[0]}' AND zm_zmina='{zinfo[1]}'")
         else:
             cur.execute(f"INSERT INTO zmina (zm_date,zm_zmina,nomer_zm,maister_id) VALUES ('{zinfo[0]}', '{zinfo[1]}', '{nomer_zm}', '{maister_id}')")
             cur.execute(f"SELECT zmina_id FROM zmina WHERE zm_date='{zinfo[0]}' AND zm_zmina='{zinfo[1]}'")
-            zm_id = cur.fetchone()[0]
+            zm_id = cur.fetchone()
 
         # going through every textures and make lists for non-empty textures 
         for i in range(1,15):
             if cur_textures[i-1] == '':
                 continue
-            pe = get_pressE(i)
+            pe = get_pressE(i, thick)
             texture_id = texture_list.index(cur_textures[i-1]) + 1      # id starts from 1 in db table
-            t_lists.append([zm_id, texture_id] + pe + request.form.getlist(f"t{i}"))
+            t_lists.append([zm_id[0], texture_id] + pe + request.form.getlist(f"t{i}"))
 
         cur.execute
         rob_zm_names = column_names['robota_zm']
@@ -97,20 +100,21 @@ def home2():
             koef, thick = 0.090585, '18'
         tinfo = request.args.getlist("top_info")
         if tinfo:
-            gen_info = tinfo
+            gen_info = tinfo + [thick]
         else: 
-            gen_info = [date.today().isoformat(), '1', maister_list[0],'1']   #додати ім'я майстра {3}
+            gen_info = [date.today().isoformat(), '1', '', '', thick]   #додати ім'я майстра {3}
         print('GETTT')
         conn = sqlite3.connect('db/lam1.db')
         cur = conn.cursor()
-        cur.execute(f"SELECT zmina_id FROM zmina WHERE zm_date='{gen_info[0]}' AND zm_zmina='{gen_info[1]}'")
+        cur.execute(f"SELECT zmina_id, nomer_zm, general_name FROM zmina INNER JOIN maister ON zmina.maister_id=maister.maister_id WHERE zm_date='{gen_info[0]}' AND zm_zmina='{gen_info[1]}'")
         zm_id = cur.fetchone()
         if zm_id:
-            zm_id = zm_id[0]
-            cur.execute(f"SELECT * FROM robota_zm WHERE zmina_id='{zm_id}'")
+            cur.execute(f"SELECT * FROM robota_zm WHERE zmina_id='{zm_id[0]}' AND thickness='{thick}'")
             c = cur.fetchall()
             for i in c:
-                t_lists.append((texture_list[i[2]-1],) + i[5:])
+                t_lists.append((texture_list[i[2]-1],) + i[6:])
+            
+            gen_info[2], gen_info[3] = zm_id[2], zm_id[1]
         else:
             pass
         conn.close()
