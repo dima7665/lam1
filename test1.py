5571d0b3edaa70f5a5aa235c2c77a4e7bcd3de33
dima7665 <dima7685@gmail.com>
2021-03-30 14:01:20 +0300

    month remainders part2

--- test1.py
+++ test1.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
 from textures import get_textures_list, get_maister_list, get_column_names
+from textures import sql_command_month_rem, sql_command_work_get
 from datetime import date
 import sqlite3
 
@@ -12,7 +13,7 @@ maister_list = get_maister_list()
 gen_info = ['2021-02-10', '2', maister_list[0], '1']
 
 column_names = dict()
-for i in ['robota_zm','remainders']:                     # додати в список інші таблиці
+for i in ['robota_zm','remainders', 'month_rem']:                     # додати в список інші таблиці
     column_names[i] = get_column_names(i)
 
 def get_pressE(i, thick):
@@ -41,6 +42,13 @@ def update_command(table, names_lst, lst):
             command += f" {names_lst[x+1]} = '{lst[x]}',"
         command = command[:-1]
         command += f" WHERE zmina_id = {lst[0]} AND textures_id = {lst[1]} AND thickness={lst[2]} AND e_quality = {lst[3]} AND source='{lst[4]}'"
+        return command
+    if table == 'month_rem':
+        command = "UPDATE month_rem SET"
+        for x in range(5,9):
+            command += f" {names_lst[x]} = '{lst[x]}',"
+        command = command[:-1]
+        command += f" WHERE month = '{lst[0]}' AND year = '{lst[1]}' AND textures_id='{lst[2]}' AND thickness = '{lst[3]}' AND e_quality='{lst[4]}'"
         return command
 
 def make_texture_lists_for_remainders(lst):
@@ -159,7 +167,6 @@ def robota():
     if request.method == 'POST':
         t_list = []
         thick, eq = request.form.get("thickness"), request.form.get("e_quality")
-        print(thick,eq)
         koef = 0.08052
         zinfo = request.form.getlist('zm_info')
         cur_textures = request.form.getlist("tvalues")
@@ -219,29 +226,7 @@ def robota():
         zm_id = cur.fetchone()
         if zm_id:
            # cur.execute(f"SELECT name,sort1,sort2,sort3,sort4 FROM robota_zm INNER JOIN textures ON robota_zm.textures_id=textures.textures_id WHERE zmina_id='{zm_id[0]}' AND thickness='{thick}' AND e_quality='{eq}'")
-            cur.execute(f"""SELECT t.name,zmina_id,thickness,e_quality,sum(zr1),sum(zr2),sum(zr3),sum(zr4),sum(sort1),sum(sort2),sum(sort3),sum(sort4), sum(zs1),sum(zs2),sum(zs3),sum(zs4), sum(zf1),sum(zf2),sum(zf3),sum(zf4),sum(ms1),sum(ms2),sum(ms3),sum(ms4)
-            FROM (SELECT textures_id,zmina_id,thickness,e_quality,NULL zr1,NULL zr2,NULL zr3,NULL zr4,sort1,sort2,sort3,sort4,NULL zs1,NULL zs2,NULL zs3,NULL zs4,NULL zf1,NULL zf2,NULL zf3,NULL zf4,NULL ms1,NULL ms2,NULL ms3,NULL ms4
-                    FROM remainders
-                    WHERE source='nas' AND zmina_id={zm_id[0]} AND thickness={thick} AND e_quality={eq}
-                UNION ALL
-                SELECT textures_id,zmina_id,thickness,e_quality,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,sort1,sort2,sort3,sort4,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL 
-                    FROM remainders
-                    WHERE source='zis' AND zmina_id={zm_id[0]} AND thickness={thick} AND e_quality={eq}
-                UNION ALL
-                SELECT textures_id,zmina_id,thickness,e_quality,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,sort1,sort2,sort3,sort4,NULL,NULL,NULL,NULL
-                    FROM remainders
-                    WHERE source='zif' AND zmina_id={zm_id[0]} AND thickness={thick} AND e_quality={eq}
-                UNION ALL
-                SELECT textures_id,zmina_id,thickness,e_quality,sort1,sort2,sort3,sort4,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL
-                    FROM robota_zm
-                    WHERE zmina_id={zm_id[0]} AND thickness={thick} AND e_quality={eq}
-                UNION ALL
-                SELECT textures_id,NULL,thickness,e_quality,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,sort1 ms1,sort2 ms2,sort3 ms3,sort4 ms4
-                    FROM month_rem
-                    WHERE month='{mdate[1]}' AND year={mdate[0]} AND thickness={thick} AND e_quality={eq})
-            INNER JOIN textures t USING(textures_id)
-            GROUP BY t.name
-            """)
+            cur.execute(sql_command_work_get.format(zm_id[0], thick, eq, mdate[1], mdate[0]))
             c = cur.fetchall()
             for i in c:
                 print(i)
@@ -263,40 +248,27 @@ def monfin():
 
         conn = sqlite3.connect("db/lam1.db")
         cur = conn.cursor()
+        
         cur.execute(f"SELECT textures_id FROM textures WHERE name='{m[2]}'")
         tid = cur.fetchone()[0]
-        print("texture id ----",tid)
-        print(m)
-        start_date, end_date = '2021-02-01','2021-02-28'
+        mon = ('01','02','03','04','05','06','07','08','09','10','11','12')[int(m[1])-2]
+        year = m[0] if int(m[1])>1 else int(m[0])-1
+        start_date, end_date = f'{year}-{mon}-01', f'{year}-{mon}-31'
         thick, eq ='16', '1'
-        cur.execute(f"""
-        SELECT tt.textures_id, st.s1+pres.s1-nas.s1+zis.s1-zif.s1 sort1, st.s2+pres.s2-nas.s2+zis.s2-zif.s2 sort2, st.s3+pres.s3-nas.s3+zis.s3-zif.s3 sort3, st.s4+pres.s4-nas.s4+zis.s4-zif.s4 sort4
-        FROM (SELECT * FROM textures WHERE textures_id={tid}) tt
-            LEFT JOIN
-            (SELECT textures_id,COALESCE(sum(sort1),0) s1,COALESCE(sum(sort2),0) s2,COALESCE(sum(sort3),0) s3,COALESCE(sum(sort4),0) s4
-                FROM remainders INNER JOIN zmina z USING(zmina_id)
-                WHERE source='nas' AND textures_id='{tid}' AND thickness='{thick}' AND e_quality='{eq}' AND zm_date BETWEEN '{start_date}' AND '{end_date}') nas
-            LEFT JOIN
-            (SELECT COALESCE(sum(sort1),0) s1,COALESCE(sum(sort2),0) s2,COALESCE(sum(sort3),0) s3,COALESCE(sum(sort4),0) s4
-                FROM remainders INNER JOIN zmina z USING(zmina_id)
-                WHERE source='zis' AND textures_id='{tid}' AND thickness='{thick}' AND e_quality='{eq}' AND zm_date BETWEEN '{start_date}' AND '{end_date}') zis
-            LEFT JOIN 
-            (SELECT COALESCE(sum(sort1),0) s1,COALESCE(sum(sort2),0) s2,COALESCE(sum(sort3),0) s3,COALESCE(sum(sort4),0) s4
-                FROM remainders INNER JOIN zmina z USING(zmina_id)
-                WHERE source='zif' AND textures_id='{tid}' AND thickness='{thick}' AND e_quality='{eq}' AND zm_date BETWEEN '{start_date}' AND '{end_date}') zif
-            LEFT JOIN
-            (SELECT COALESCE(sum(sort1),0) s1,COALESCE(sum(sort2),0) s2,COALESCE(sum(sort3),0) s3,COALESCE(sum(sort4),0) s4
-                FROM month_rem 
-                WHERE month='{m[1]}' AND year='{m[0]}' AND textures_id='{tid}' AND thickness='{thick}' AND e_quality='{eq}') st
-            LEFT JOIN
-            (SELECT COALESCE(sum(sort1),0) s1,COALESCE(sum(sort2),0) s2,COALESCE(sum(sort3),0) s3,COALESCE(sum(sort4),0) s4
-                FROM robota_zm INNER JOIN zmina z USING(zmina_id)
-                WHERE textures_id='{tid}' AND thickness='{thick}' AND e_quality='{eq}' AND zm_date BETWEEN '{start_date}' AND '{end_date}') pres
-        """)
+        # textures_id, thickness, e_quality, start_date, end_date, month, year
+        cur.execute(sql_command_month_rem.format(tid, thick, eq, start_date, end_date, mon, year))
         res = cur.fetchone()
-        print(res)
-
-        message = f'{m[0]}-{m[1]}  complete --- {m[2]} --- {res}'
+        ins = [m[1], m[0], res[0], thick, eq] + list(res[1:])  # порядок стовпців у таблиці month_rem
+        print(ins)
+        month_rem_names = column_names['month_rem']
+        
+        try:
+            cur.execute(f"INSERT INTO month_rem {month_rem_names[1:]} VALUES {tuple(ins)}")
+        except sqlite3.IntegrityError:
+            command = update_command('month_rem', month_rem_names[1:], ins)
+            cur.execute(command)
+        conn.commit()
+        message = f"{m[0]}-{m[1]}  complete --- {m[2]} --- {res}"
         return render_template('monthfinisher.html', maister_list=maister_list, texture_list=texture_list, message=message)   
 
 
--- textures.py
+++ textures.py
@@ -42,3 +42,52 @@ def get_column_names(table):
 	res = tuple([i[0] for i in c.description])
 	conn.close()
 	return res
+
+sql_command_month_rem = """
+        SELECT tt.textures_id, st.s1+pres.s1-nas.s1+zis.s1-zif.s1 sort1, st.s2+pres.s2-nas.s2+zis.s2-zif.s2 sort2, st.s3+pres.s3-nas.s3+zis.s3-zif.s3 sort3, st.s4+pres.s4-nas.s4+zis.s4-zif.s4 sort4
+        FROM (SELECT * FROM textures WHERE textures_id={0}) tt
+            LEFT JOIN
+            (SELECT textures_id,COALESCE(sum(sort1),0) s1,COALESCE(sum(sort2),0) s2,COALESCE(sum(sort3),0) s3,COALESCE(sum(sort4),0) s4
+                FROM remainders INNER JOIN zmina z USING(zmina_id)
+                WHERE source='nas' AND textures_id='{0}' AND thickness='{1}' AND e_quality='{2}' AND zm_date BETWEEN '{3}' AND '{4}') nas
+            LEFT JOIN
+            (SELECT COALESCE(sum(sort1),0) s1,COALESCE(sum(sort2),0) s2,COALESCE(sum(sort3),0) s3,COALESCE(sum(sort4),0) s4
+                FROM remainders INNER JOIN zmina z USING(zmina_id)
+                WHERE source='zis' AND textures_id='{0}' AND thickness='{1}' AND e_quality='{2}' AND zm_date BETWEEN '{3}' AND '{4}') zis
+            LEFT JOIN 
+            (SELECT COALESCE(sum(sort1),0) s1,COALESCE(sum(sort2),0) s2,COALESCE(sum(sort3),0) s3,COALESCE(sum(sort4),0) s4
+                FROM remainders INNER JOIN zmina z USING(zmina_id)
+                WHERE source='zif' AND textures_id='{0}' AND thickness='{1}' AND e_quality='{2}' AND zm_date BETWEEN '{3}' AND '{4}') zif
+            LEFT JOIN
+            (SELECT COALESCE(sum(sort1),0) s1,COALESCE(sum(sort2),0) s2,COALESCE(sum(sort3),0) s3,COALESCE(sum(sort4),0) s4
+                FROM month_rem 
+                WHERE month='{5}' AND year='{6}' AND textures_id='{0}' AND thickness='{1}' AND e_quality='{2}') st
+            LEFT JOIN
+            (SELECT COALESCE(sum(sort1),0) s1,COALESCE(sum(sort2),0) s2,COALESCE(sum(sort3),0) s3,COALESCE(sum(sort4),0) s4
+                FROM robota_zm INNER JOIN zmina z USING(zmina_id)
+                WHERE textures_id='{0}' AND thickness='{1}' AND e_quality='{2}' AND zm_date BETWEEN '{3}' AND '{4}') pres
+        """
+
+sql_command_work_get = """SELECT t.name,zmina_id,thickness,e_quality,sum(zr1),sum(zr2),sum(zr3),sum(zr4),sum(sort1),sum(sort2),sum(sort3),sum(sort4), sum(zs1),sum(zs2),sum(zs3),sum(zs4), sum(zf1),sum(zf2),sum(zf3),sum(zf4),sum(ms1),sum(ms2),sum(ms3),sum(ms4)
+            FROM (SELECT textures_id,zmina_id,thickness,e_quality,NULL zr1,NULL zr2,NULL zr3,NULL zr4,sort1,sort2,sort3,sort4,NULL zs1,NULL zs2,NULL zs3,NULL zs4,NULL zf1,NULL zf2,NULL zf3,NULL zf4,NULL ms1,NULL ms2,NULL ms3,NULL ms4
+                    FROM remainders
+                    WHERE source='nas' AND zmina_id={0} AND thickness={1} AND e_quality={2}
+                UNION ALL
+                SELECT textures_id,zmina_id,thickness,e_quality,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,sort1,sort2,sort3,sort4,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL 
+                    FROM remainders
+                    WHERE source='zis' AND zmina_id={0} AND thickness={1} AND e_quality={2}
+                UNION ALL
+                SELECT textures_id,zmina_id,thickness,e_quality,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,sort1,sort2,sort3,sort4,NULL,NULL,NULL,NULL
+                    FROM remainders
+                    WHERE source='zif' AND zmina_id={0} AND thickness={1} AND e_quality={2}
+                UNION ALL
+                SELECT textures_id,zmina_id,thickness,e_quality,sort1,sort2,sort3,sort4,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL
+                    FROM robota_zm
+                    WHERE zmina_id={0} AND thickness={1} AND e_quality={2}
+                UNION ALL
+                SELECT textures_id,NULL,thickness,e_quality,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,sort1 ms1,sort2 ms2,sort3 ms3,sort4 ms4
+                    FROM month_rem
+                    WHERE month='{3}' AND year={4} AND thickness={1} AND e_quality={2})
+            INNER JOIN textures t USING(textures_id)
+            GROUP BY t.name
+            """
