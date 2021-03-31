32f79807b85cec9b1da8a6cf04cf202fd37ee875
dima7665 <dima7685@gmail.com>
2021-03-31 14:51:42 +0300

    month remainders part3 - nothing to insert/update check

--- test1.py
+++ test1.py
@@ -245,30 +245,44 @@ def monfin():
         return render_template('monthfinisher.html', maister_list=maister_list, texture_list=texture_list) 
     if request.method == "POST":
         m = request.form.getlist('mfin')
+        month_rem_names = column_names['month_rem']
 
-        conn = sqlite3.connect("db/lam1.db")
-        cur = conn.cursor()
-        
-        cur.execute(f"SELECT textures_id FROM textures WHERE name='{m[2]}'")
-        tid = cur.fetchone()[0]
+        # month and year for select all 
         mon = ('01','02','03','04','05','06','07','08','09','10','11','12')[int(m[1])-2]
         year = m[0] if int(m[1])>1 else int(m[0])-1
-        start_date, end_date = f'{year}-{mon}-01', f'{year}-{mon}-31'
+        start_date, end_date = f'{year}-{mon}-01', f'{year}-{mon}-31'          
+        conn = sqlite3.connect("db/lam1.db")
+        cur = conn.cursor()
         thick, eq ='16', '1'
-        # textures_id, thickness, e_quality, start_date, end_date, month, year
-        cur.execute(sql_command_month_rem.format(tid, thick, eq, start_date, end_date, mon, year))
-        res = cur.fetchone()
-        ins = [m[1], m[0], res[0], thick, eq] + list(res[1:])  # порядок стовпців у таблиці month_rem
-        print(ins)
-        month_rem_names = column_names['month_rem']
-        
-        try:
-            cur.execute(f"INSERT INTO month_rem {month_rem_names[1:]} VALUES {tuple(ins)}")
-        except sqlite3.IntegrityError:
-            command = update_command('month_rem', month_rem_names[1:], ins)
-            cur.execute(command)
-        conn.commit()
-        message = f"{m[0]}-{m[1]}  complete --- {m[2]} --- {res}"
+        for tex_name in ['Аляска']:
+            cur.execute(f"SELECT textures_id FROM textures WHERE name='{m[2]}'")
+            tid = cur.fetchone()[0]
+
+            # textures_id, thickness, e_quality, start_date, end_date, month, year
+            cur.execute(sql_command_month_rem.format(tid, thick, eq, start_date, end_date, mon, year))
+            res = cur.fetchone()
+            currecord = cur.execute(f"SELECT sort1,sort2,sort3,sort4 FROM month_rem WHERE month='{m[1]}' AND year='{m[0]}' AND textures_id='{tid}' AND thickness='{thick}' AND e_quality='{eq}'").fetchone()
+
+            do = False
+            if not any(res[1:]):                # if movement record [0,0,0,0]
+                if currecord:                   # and current record exist
+                    if any(currecord):       # and current record is not [0,0,0,0]
+                        do = True
+            else:                               # if movement record is not [0,0,0,0]
+                do = True
+                if currecord:
+                    if res[1:] == currecord:
+                        do = False
+            message = "No update in db"
+            if do:
+                ins = [m[1], m[0], res[0], thick, eq] + list(res[1:])  # порядок стовпців у таблиці month_rem
+                try:
+                    cur.execute(f"INSERT INTO month_rem {month_rem_names[1:]} VALUES {tuple(ins)}")
+                except sqlite3.IntegrityError:
+                    command = update_command('month_rem', month_rem_names[1:], ins)
+                    cur.execute(command)
+                conn.commit()
+                message = f"{m[0]}-{m[1]}  complete --- {m[2]} --- {currecord} ===> {res[1:]}"
         return render_template('monthfinisher.html', maister_list=maister_list, texture_list=texture_list, message=message)   
 
 
